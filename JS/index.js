73edb12e97a6e5979bf70da24a16a99855b360f1
Wiam <wiambal21@gmail.com>
2024-12-11 23:10:59 +0100

    rewrtie

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,13 @@
-function hideBlok(){
+const inputField = document.getElementById("inputField");
+const container = document.getElementById("container-tasks"); 
+
+function planning(){
+    if(inputField === ''){
+        alert("Please add your text!");
+    } else{
+        let li = document.createElement("li");
+        li.innerHTML = inputField.value + '';
+        container.appendChild(li);
+    }
     
 }
